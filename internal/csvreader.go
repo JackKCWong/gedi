9bdab26d7d6ebeadd29a63987a0872b8b03568b0
JackKCWong <jiajue.huang@live.com>
2024-08-07 21:29:11 +0800

    add cyclic buffer for csv

--- internal/csvreader.go
+++ internal/csvreader.go
@@ -13,6 +13,8 @@ var _ = (RecordReader)((*CsvReader)(nil))
 type CsvReader struct {
 }
 
+const BUF_SIZE = 4096
+
 // Read a csv file and return a `chan []string` of rows and cells
 func (c *CsvReader) Read(r io.Reader) (chan Record, error) {
 	rawBuf := bytes.Buffer{}
@@ -22,6 +24,7 @@ func (c *CsvReader) Read(r io.Reader) (chan Record, error) {
 	go func() {
 		defer close(records)
 		var lastOffset int64
+		var shift int64
 		for {
 			record, err := csvRd.Read()
 			if err == io.EOF {
@@ -33,12 +36,21 @@ func (c *CsvReader) Read(r io.Reader) (chan Record, error) {
 				continue
 			}
 
+			raw := rawBuf.String()[lastOffset:csvRd.InputOffset()-shift]
 			records <- Record{
-				raw:    strings.TrimRight(rawBuf.String()[lastOffset:csvRd.InputOffset()], "\r\n"),
+				raw:    strings.TrimRight(raw, "\r\n"),
 				parsed: record,
 			}
 
-			lastOffset = csvRd.InputOffset()
+			if lastOffset > BUF_SIZE {
+				remain := rawBuf.String()[csvRd.InputOffset()-shift:]
+				rawBuf.Reset()
+				rawBuf.WriteString(remain)
+				shift = csvRd.InputOffset()
+				lastOffset = 0
+			} else {
+				lastOffset = csvRd.InputOffset() - shift
+			}
 		}
 	}()
 
